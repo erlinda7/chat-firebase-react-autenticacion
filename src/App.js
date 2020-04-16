7a53ba5fefd3e191442f5bd544dba0e7b87a2d53
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-16 14:53:49 -0400

    configuracion inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import {auth} from './services/firebase';
 
 function App() {
   return (
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,1 +1,17 @@
 import firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyCzrWzsz4GBFHvji1wiJdUTOn-Wi-mI6uE",
+    authDomain: "miprimerproyecto-fa3bf.firebaseapp.com",
+    databaseURL: "https://miprimerproyecto-fa3bf.firebaseio.com",
+    projectId: "miprimerproyecto-fa3bf",
+    storageBucket: "miprimerproyecto-fa3bf.appspot.com",
+    messagingSenderId: "312821841245",
+    appId: "1:312821841245:web:a0a0422bc6d7a1117a3cb4",
+    measurementId: "G-3VXJN3Z8FE"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default auth = firebase.auth;
+export default db = firebase.database();
